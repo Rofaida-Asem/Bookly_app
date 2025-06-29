41792ab938e3eb15b6900eca11c72d0ee663c1aa
Rofaida-Asem <107304774+Rofaida-Asem@users.noreply.github.com>
2025-06-29 16:05:44 +0300

    best seller item part 2

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -11,7 +11,7 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      padding: const EdgeInsets.symmetric(horizontal: 30.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -23,6 +23,9 @@ class HomeViewBody extends StatelessWidget {
           Text(
             'Best Seller',
             style: Styles.textStyle18,
+          ),
+          const SizedBox(
+            height: 20,
           ),
           BestSellerListViewItem(),
         ],
@@ -53,7 +56,22 @@ class BestSellerListViewItem extends StatelessWidget {
               ),
             ),
           ),
-          Column(),
+          const SizedBox(
+            width: 30,
+          ),
+          Column(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .5,
+                child: Text(
+                  'Harry Potter and the Goblet of Fire',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: Styles.textStyle20,
+                ),
+              )
+            ],
+          ),
         ],
       ),
     );
