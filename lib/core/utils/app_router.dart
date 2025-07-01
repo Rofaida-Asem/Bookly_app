0083977ec232ad85d1731eca36f850f3ebf0b114
Rofaida-Asem <107304774+Rofaida-Asem@users.noreply.github.com>
2025-07-01 13:10:55 +0300

    create search view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,13 +1,19 @@
 import 'package:bookly_app/Features/home/presentation/views/book_details_view.dart';
 import 'package:bookly_app/Features/home/presentation/views/home_view.dart';
+import 'package:bookly_app/Features/search/presentation/views/search_view.dart';
 import 'package:bookly_app/Features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const KHomeView = '/homeView';
   static const KBookDetailsView = '/bookDetailsView';
+  static const KSearchView = '/searchView';
   static final router = GoRouter(
     routes: [
+      GoRoute(
+        path: KSearchView,
+        builder: (context, state) => SearchView(),
+      ),
       GoRoute(
         path: '/',
         builder: (context, state) => SplashView(),
